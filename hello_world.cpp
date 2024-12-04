381a578d15b098fb2667579894e56e5e37a87738
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2024-12-04 21:07:21 +0300

    Added user import for name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
+int main() {
+    string name;
+    cout<<"Enter your name";
+    cin>>name;
     cout << "Hello, World!\n";
     return 0;
 }
