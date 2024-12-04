cf6cc072fdcc04a36b82ace1767a3f7ac72c192c
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2024-12-04 21:15:50 +0300

    Removed using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-    string name;
-    cout<<"Enter your name";
-    cin>>name;
-    cout << "Hello, World!\n";
+    std::string name;
+    std::cout<<"Enter your name";
+    std::cin>>name;
+    std::cout << "Hello, World!\n";
     return 0;
 }
